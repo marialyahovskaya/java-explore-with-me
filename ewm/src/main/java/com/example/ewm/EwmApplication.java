9460b74e6f361f12ebc47ea09d568a18a0e580ea
Maria Lyakhovskaya <mariyalyakhovskaya@yandex.ru>
2023-01-26 22:18:48 +0300

    Настроен многомодульный проект

---
+++ ewm/src/main/java/com/example/ewm/EwmApplication.java
@@ -0,0 +1,13 @@
+package com.example.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwmApplication.class, args);
+    }
+
+}
